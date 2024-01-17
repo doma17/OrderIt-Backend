acd77d5d2fdc765e71d9aff49f942353fc287756
doma17 <rovin1273@gmail.com>
2024-01-17 13:33:02 +0900

    Initial commit

---
+++ src/main/java/inu/amigo/order_it/OrderItApplication.java
@@ -0,0 +1,13 @@
+package inu.amigo.order_it;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderItApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderItApplication.class, args);
+    }
+
+}
---
+++ src/test/java/inu/amigo/order_it/OrderItApplicationTests.java
@@ -0,0 +1,13 @@
+package inu.amigo.order_it;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderItApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
