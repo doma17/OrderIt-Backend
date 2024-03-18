8731afe05c4738aacf372a050adfe4686e4ef2a8
doma17 <rovin1273@gmail.com>
2024-03-19 00:35:26 +0900

    feat(Order) : Order 서비스 생성2

--- src/main/java/inu/amigo/order_it/order/entity/Order.java
+++ src/main/java/inu/amigo/order_it/order/entity/Order.java
@@ -1,15 +1,28 @@
 package inu.amigo.order_it.order.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "orders") // order은 SQL 예약어
 @Entity
 public class Order {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    
+    @OneToMany(cascade = CascadeType.ALL)
+    private List<OrderItem> orderItems = new ArrayList<>();
+
+    private int totalPrice;
 }
--- src/main/java/inu/amigo/order_it/order/entity/OrderItem.java
+++ src/main/java/inu/amigo/order_it/order/entity/OrderItem.java
@@ -1,6 +1,25 @@
 package inu.amigo.order_it.order.entity;
 
+import inu.amigo.order_it.item.entity.Item;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
 public class OrderItem {
 
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
+    @ManyToOne
+    private Item item;
+
+    private int quantity;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Order order;
 }
