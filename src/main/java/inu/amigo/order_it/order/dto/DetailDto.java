d10b017373d726132a875ac8f6e576617fbb223d
doma17 <rovin1273@gmail.com>
2024-04-01 19:21:28 +0900

    add : OrderDto Swagger Schema 추가

--- src/main/java/inu/amigo/order_it/order/dto/DetailDto.java
+++ src/main/java/inu/amigo/order_it/order/dto/DetailDto.java
@@ -1,12 +1,15 @@
 package inu.amigo.order_it.order.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter @Setter
 public class DetailDto {
 
+    @Schema(description = "주문 Item의 아이디", example = "1")
     private Long itemId;
 
+    @Schema(description = "주문 Item의 개수", example = "3")
     private int quantity;
 }
--- src/main/java/inu/amigo/order_it/order/dto/OrderDto.java
+++ src/main/java/inu/amigo/order_it/order/dto/OrderDto.java
@@ -1,5 +1,6 @@
 package inu.amigo.order_it.order.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -8,9 +9,12 @@ import java.util.List;
 @Getter @Setter
 public class OrderDto {
 
+    @Schema(description = "주문한 Item 배열")
     private List<DetailDto> detailDtoList;
 
+    @Schema(description = "주문의 총 가격", example = "4500")
     private int totalPrice;
 
+    @Schema(description = "주문의 OrderType", example = "TAKE_OUT")
     private OrderType orderType;
 }
